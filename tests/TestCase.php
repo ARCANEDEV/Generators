341ae7ddbb2bb78804c060f3045fd0b0daa1bcf0
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-20 02:35:08 +0000

    Adding the base Test case

---
+++ tests/TestCase.php
@@ -0,0 +1,57 @@
+<?php namespace Arcanedev\Generators\Tests;
+
+use Arcanedev\Generators\GeneratorsServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Your code here
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            GeneratorsServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Get package aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [];
+    }
+}
