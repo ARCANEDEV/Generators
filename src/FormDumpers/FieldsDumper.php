11df1276b8db499de2b6e21061dc247f6dd154f2
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-20 02:33:50 +0000

    Adding form dumpers

---
+++ src/FormDumpers/FieldsDumper.php
@@ -0,0 +1,145 @@
+<?php namespace Arcanedev\Generators\FormDumpers;
+
+use Arcanedev\Generators\Migrations\SchemaParser;
+use Arcanedev\Generators\Stub;
+use Arcanedev\Generators\Traits\StubTrait;
+
+/**
+ * Class FieldsDumper
+ * @package Arcanedev\Generators\FormDumpers
+ */
+class FieldsDumper
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Traits
+     | ------------------------------------------------------------------------------------------------
+     */
+    use StubTrait;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The form fields.
+     *
+     * @var string
+     */
+    protected $fields;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The constructor.
+     *
+     * @param string $fields
+     */
+    public function __construct($fields)
+    {
+        $this->fields = $fields;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get schema parser.
+     *
+     * @return SchemaParser
+     */
+    public function getParser()
+    {
+        return new SchemaParser($this->fields);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Render the form.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $results = '';
+
+        foreach ($this->getParser()->toArray() as $name => $types) {
+            $results .= $this->getStub($this->getFieldType($types), $name).PHP_EOL;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Convert the fields to html heading.
+     *
+     * @return string
+     */
+    public function toHeading()
+    {
+        $results = '';
+
+        foreach ($this->getParser()->toArray() as $name => $types) {
+            if (in_array($name, $this->ignores)) {
+                continue;
+            }
+
+            $results .= "\t\t\t" . '<th>' . ucwords($name) . '</th>' . PHP_EOL;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Convert the fields to formatted php script.
+     *
+     * @param string $var
+     *
+     * @return string
+     */
+    public function toBody($var)
+    {
+        $results = '';
+
+        foreach ($this->getParser()->toArray() as $name => $types) {
+            if (in_array($name, $this->ignores)) {
+                continue;
+            }
+
+            $results .= "\t\t\t\t\t" . '<td>{!! $' . $var . '->' . $name . ' !!}</td>' . PHP_EOL;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Get replacements for $SHOW_BODY$.
+     *
+     * @param string $var
+     *
+     * @return string
+     */
+    public function toRows($var)
+    {
+        $results = PHP_EOL;
+
+        foreach ($this->getParser()->toArray() as $name => $types) {
+            if (in_array($name, $this->ignores)) {
+                continue;
+            }
+
+            $results .= Stub::createFromPath(__DIR__.'/../../stubs/scaffold/row.stub', [
+                'label'     => ucwords($name),
+                'column'    => $name,
+                'var'       => $var,
+            ])->render();
+        }
+
+        return $results.PHP_EOL;
+    }
+}
---
+++ src/FormDumpers/TableDumper.php
@@ -0,0 +1,167 @@
+<?php namespace Arcanedev\Generators\FormDumpers;
+
+use Arcanedev\Generators\Traits\StubTrait;
+use Doctrine\DBAL\Schema\Column;
+use Illuminate\Support\Facades\DB;
+use Arcanedev\Generators\Stub;
+
+/**
+ * Class TableDumper
+ * @package Arcanedev\Generators\FormDumpers
+ */
+class TableDumper
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Traits
+     | ------------------------------------------------------------------------------------------------
+     */
+    use StubTrait;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The constructor.
+     *
+     * @param string $table
+     */
+    public function __construct($table)
+    {
+        $this->table = $table;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get table name.
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * Get column.
+     *
+     * @return array
+     */
+    public function getColumns()
+    {
+        return DB::getDoctrineSchemaManager()
+            ->listTableDetails($this->getTable())
+            ->getColumns();
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Render the form.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $columns = $this->getColumns();
+
+        $results = '';
+
+        foreach ($columns as $column) {
+            /** @var Column $column */
+            $results .= $this->getStub(
+                $column->getType()->getName(),
+                $column->getName()
+            );
+        }
+
+        return $results;
+    }
+
+    /**
+     * Convert the fields to html heading.
+     *
+     * @return string
+     */
+    public function toHeading()
+    {
+        $results = '';
+
+        foreach ($this->getColumns() as $column) {
+            /** @var Column $column */
+            if (in_array($name = $column->getName(), $this->ignores)) {
+                continue;
+            }
+
+            $results .= "\t\t\t" . '<th>' . ucwords($name) . '</th>' . PHP_EOL;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Convert the fields to formatted php script.
+     *
+     * @param string $var
+     *
+     * @return string
+     */
+    public function toBody($var)
+    {
+        $results = '';
+
+        foreach ($this->getColumns() as $column) {
+            /** @var Column $column */
+            if (in_array($name = $column->getName(), $this->ignores)) {
+                continue;
+            }
+
+            $results .= "\t\t\t\t\t".'<td>{!! $'.$var.'->'.$name.' !!}</td>' . PHP_EOL;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Get replacements for $SHOW_BODY$.
+     *
+     * @param string $var
+     *
+     * @return string
+     */
+    public function toRows($var)
+    {
+        $results = PHP_EOL;
+
+        foreach ($this->getColumns() as $column) {
+            /** @var Column $column */
+            if (in_array($name = $column->getName(), $this->ignores)) {
+                continue;
+            }
+
+            $results .= Stub::create(__DIR__ . '/../../stubs/scaffold/row.stub', [
+                'label'  => ucwords($name),
+                'column' => $name,
+                'var'    => $var,
+            ])->render();
+        }
+
+        return $results . PHP_EOL;
+    }
+}
