39cfaa265647ac54409ac4ad64a1620e073641a1
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-20 02:35:19 +0000

    Adding the service provider

---
+++ src/GeneratorsServiceProvider.php
@@ -0,0 +1,37 @@
+<?php namespace Arcanedev\Generators;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class GeneratorsServiceProvider
+ * @package Arcanedev\Generators
+ */
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $commands = [
+            Commands\ModelCommand::class,
+            Commands\ControllerCommand::class,
+            Commands\ConsoleCommand::class,
+            Commands\ViewCommand::class,
+            Commands\SeedCommand::class,
+            Commands\MigrationCommand::class,
+            Commands\RequestCommand::class,
+            Commands\PivotCommand::class,
+            Commands\ScaffoldCommand::class,
+            Commands\FormCommand::class,
+        ];
+
+        foreach ($commands as $command) {
+            $this->commands($command);
+        }
+    }
+}
