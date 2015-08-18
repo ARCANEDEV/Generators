4e3b7a31c3d176d27e409f249c471c643eea889b
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-08-18 18:57:52 +0100

    Adding Exception

--- src/Exceptions/FileAlreadyExistsException.php
+++ src/Exceptions/FileAlreadyExistsException.php
@@ -1,3 +1,7 @@
 <?php namespace Arcanedev\Generators\Exceptions;
 
+/**
+ * Class FileAlreadyExistsException
+ * @package Arcanedev\Generators\Exceptions
+ */
 class FileAlreadyExistsException extends \Exception {}
